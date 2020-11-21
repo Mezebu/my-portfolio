24ef9ac308d3275341e869fd9554582d4e783089
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2020-11-21 23:26:06 +0200

    Added contact image

--- src/components/contact.js
+++ src/components/contact.js
@@ -10,9 +10,9 @@ class Contact extends Component {
                    <Cell col={6}>
                        <h2>Ezeigwe Henry</h2>
                        <img 
-                         src='https://cdn2.iconfinder.com/data/icons/avatar-2/512/Fred_man-512.png'
+                         src='https://cdn.dribbble.com/users/1397292/screenshots/3855289/icon-1.gif'
                          alt='profile-pic'
-                         style={{height: '250px'}}
+                         style={{height: '350px'}}
                        />
                    </Cell>
                    <Cell col={6}>
@@ -20,13 +20,14 @@ class Contact extends Component {
                        <hr/>
                        <div className='contact-list'>
                        <List>
+                           {/* Phone number */}
                          <ListItem>
                             <ListItemContent style={{fontSize: '30px', fontFamily: 'lato'}}>
                                 <i className='fa fa-phone-square' aria-hidden='true'/>
                                    09038233089
                                 </ListItemContent>
                          </ListItem>
-                         
+                            {/* Email */}
                          <ListItem>
                             <ListItemContent style={{fontSize: '30px', fontFamily: 'lato'}}>
                             <a href='mailto:mezebu07@gmail.com' rel='noreferrer noopener' target='_blank'>
@@ -35,7 +36,7 @@ class Contact extends Component {
                                  </a>
                                 </ListItemContent>
                          </ListItem>
-
+                              {/* Twitter */}
                          <ListItem>
                             <ListItemContent style={{fontSize: '30px', fontFamily: 'lato'}}>
                             <a href='https://twitter.com/Mezebu' rel='noreferrer noopener' target='_blank'>
@@ -44,7 +45,7 @@ class Contact extends Component {
                                  </a>
                                 </ListItemContent>
                          </ListItem>
-
+                               {/* Whatsappp */}
                          <ListItem>
                             <ListItemContent style={{fontSize: '30px', fontFamily: 'lato'}}>
                             <a href='https://wa.me/+2349038233089' rel='noreferrer noopener' target='_blank'>
@@ -53,7 +54,15 @@ class Contact extends Component {
                                  </a>
                                 </ListItemContent>
                          </ListItem>
-
+                               {/* Telegram */}
+                               <ListItem>
+                            <ListItemContent style={{fontSize: '30px', fontFamily: 'lato'}}>
+                            <a href='https://t.me/Mezebu' rel='noreferrer noopener' target='_blank'>
+                             <i class="fa fa-telegram" aria-hidden='true'></i>
+                                 Telegram
+                                 </a>
+                                </ListItemContent>
+                         </ListItem>
                        </List>
                        </div>
                    </Cell>
