0a233b7edd4c2a242b131c4db70f391aff213d1b
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2020-11-24 06:56:36 +0200

    Updated

--- src/components/contact.js
+++ src/components/contact.js
@@ -5,14 +5,14 @@ import { Grid, Cell, List, ListItem, ListItemContent } from 'react-mdl';
 class Contact extends Component {
     render() { 
         return ( 
-            <div className='contact-body'>
+            <div className='contact-body' id='primary'>
                <Grid className='contact-grid'>
                    <Cell col={6}>
                        <h2>Ezeigwe Henry</h2>
                        <img 
                          src='https://cdn.dribbble.com/users/736825/screenshots/6483816/cloudinary_800x600-01.jpg'
                          alt='profile-pic'
-                         style={{height: '75%', width: '90%'}}
+                         
                        />
                    </Cell>
                    <Cell col={6}>
@@ -22,14 +22,14 @@ class Contact extends Component {
                        <List>
                            {/* Phone number */}
                          <ListItem>
-                            <ListItemContent style={{fontSize: '30px', fontFamily: 'lato'}}>
+                            <ListItemContent className='contact-list-content'>
                                 <i className='fa fa-phone-square' aria-hidden='true'/>
                                    09038233089
                                 </ListItemContent>
                          </ListItem>
                             {/* Email */}
                          <ListItem>
-                            <ListItemContent style={{fontSize: '30px', fontFamily: 'lato'}}>
+                            <ListItemContent className='contact-list-content'>
                             <a href='mailto:mezebu07@gmail.com' rel='noreferrer noopener' target='_blank'>
                                  <i class="fa fa-envelope-square" aria-hidden='true' />
                                  Email
@@ -38,7 +38,7 @@ class Contact extends Component {
                          </ListItem>
                               {/* Twitter */}
                          <ListItem>
-                            <ListItemContent style={{fontSize: '30px', fontFamily: 'lato'}}>
+                            <ListItemContent className='contact-list-content'>
                             <a href='https://twitter.com/Mezebu' rel='noreferrer noopener' target='_blank'>
                                  <i class="fa fa-twitter-square" aria-hidden='true' />
                                  Twitter
@@ -47,7 +47,7 @@ class Contact extends Component {
                          </ListItem>
                                {/* Whatsappp */}
                          <ListItem>
-                            <ListItemContent style={{fontSize: '30px', fontFamily: 'lato'}}>
+                            <ListItemContent className='contact-list-content'>
                             <a href='https://wa.me/+2349038233089' rel='noreferrer noopener' target='_blank'>
                              <i class="fa fa-whatsapp" aria-hidden='true'></i>
                                  WhatsApp
@@ -56,7 +56,7 @@ class Contact extends Component {
                          </ListItem>
                                {/* Telegram */}
                                <ListItem>
-                            <ListItemContent style={{fontSize: '30px', fontFamily: 'lato'}}>
+                            <ListItemContent className='contact-list-content'>
                             <a href='https://t.me/Mezebu' rel='noreferrer noopener' target='_blank'>
                              <i class="fa fa-telegram" aria-hidden='true'></i>
                                  Telegram
